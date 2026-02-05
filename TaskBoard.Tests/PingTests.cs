ed4ef3b161230b8b59f9648f0b2479bf42a13b3b
austincripe <aaustinc@live.com>
2026-02-05 16:32:05 -0500

    first endpoint test

---
+++ TaskBoard.Tests/PingTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit.Abstractions;
+
+namespace TaskBoard.Tests;
+
+public class PingTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private readonly ITestOutputHelper _output;
+
+    public PingTests(WebApplicationFactory<Program> factory, ITestOutputHelper output)
+    {
+        _client = factory.CreateClient();
+        _output = output;
+    }
+
+    [Fact]
+    public async Task GET_Ping_Returns_Pong()
+    {
+        var response = await _client.GetAsync("/ping");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);   
+
+        var body = await response.Content.ReadAsStringAsync();
+        Assert.Equal("pong", body);
+
+    }
+}
